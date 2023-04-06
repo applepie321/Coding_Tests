9482168cf9908d10d2a7be63230aecb75b032d39
applepie321 <apple00551155@gmail.com>
2023-04-06 22:40:21 +0900

    0406

---
+++ python/The Big Book of Small Python Projects/Bagels.py
@@ -0,0 +1,76 @@
+import random
+
+NUM_DIGITS = 3
+MAX_GUESSES = 10
+
+def main():
+    print("{}digits".format(NUM_DIGITS))
+
+    while True:
+        secretNum = getSecretNum()
+        print(' You have {} guesses to get it.'.format(MAX_GUESSES))
+        
+        numGuesses = 1
+        while numGuesses <= MAX_GUESSES:
+            guess = ''
+            while len(guess) != NUM_DIGITS or not guess.isdecimal():
+                print('Guess #{}: '.format(numGuesses))
+                guess = input('> ')
+
+            clues = getClues(guess, secretNum)
+            print(clues)
+            numGuesses += 1
+
+            if guess == secretNum:
+                break
+            if numGuesses > MAX_GUESSES:
+                print('You ran out of guesses.')
+                print('The answer was {}.'.format(secretNum))
+        
+        print('Do you want to play again? (yes or no)')
+        if not input('> ').lower().startswith('y'):
+            break
+    print('Thanks for playing!')
+
+
+def getSecretNum():
+    numbers = list('0123456789')
+    random.shuffle(numbers)
+
+    secretNum = ''
+    for i in range(NUM_DIGITS):
+        secretNum += str(numbers[i])
+    return secretNum
+
+
+def getClues(guess, secretNum):
+    if guess == secretNum:
+        return 'You got it!'
+    
+    clues = []
+
+    for i in range(len(guess)):
+        if guess[i] == secretNum[i]:
+            clues.append('Fermi')
+        elif guess[i] in secretNum:
+            clues.append('Pico')
+    if len(clues) == 0:
+        return 'Bagels'
+    else:
+        clues.sort()
+        return ' '. join(clues)
+    
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+"""Bagels, by Al Sweigart al@inventwithpython.com
+A deductive logic game where you must guess a number based on clues.
+This code is available at https://nostarch.com/big-book-small-python-programming
+A version of this game is featured in the book, "Invent Your Own
+Computer Games with Python" https://nostarch.com/inventwithpython
+Tags: short, game, puzzle"""
