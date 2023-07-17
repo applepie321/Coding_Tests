4bcc112ab0b933ee11a4621e68299618a8ecb372
applepie321 <112625800+applepie321@users.noreply.github.com>
2023-07-17 23:31:16 +0900

    Create moves_in)squared_strings.rs

---
+++ rust/moves_in)squared_strings.rs
@@ -0,0 +1,16 @@
+Moves in squared strings
+
+https://www.codewars.com/kata/56dbe0e313c2f63be4000b25/solutions/rust
+
+
+
+fn hor_mirror(s: String) -> String {
+    s.split('\n').rev().collect::<Vec<&str>>().join("\n")
+}
+fn vert_mirror(s: String) -> String {
+    s.split('\n').map(|s| s.chars().rev().collect::<String>()).collect::<Vec<String>>().join("\n")
+}
+fn oper(oper: fn(String) -> String, s: String) -> String {
+    oper(s)
+}
+2 similar
