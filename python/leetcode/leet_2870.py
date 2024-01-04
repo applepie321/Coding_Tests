a68c6b9450521d4dd10e113ce70b134ecfc422ad
applepie321 <112625800+applepie321@users.noreply.github.com>
2024-01-04 22:18:51 +0900

    leet

---
+++ python/leetcode/leet_2870.py
@@ -0,0 +1,20 @@
+# Minimum Number of Operations to Make Array Empty
+# https://leetcode.com/problems/minimum-number-of-operations-to-make-array-empty/description/?envType=daily-question&envId=2024-01-04
+
+from collections import Counter
+
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+
+        frequency = Counter(nums)
+
+        count = 0
+        for i in frequency.values():
+            if i == 1:
+                return -1
+            count += i // 3
+
+            if i % 3:
+                count += 1
+        return count
