b420139fe15e204866f52a6fc8e9d166b59c030a
applepie321 <112625800+applepie321@users.noreply.github.com>
2024-05-06 21:33:47 +0900

    leet_2487.py

---
+++ python/leetcode/leet_2487.py
@@ -0,0 +1,24 @@
+def remove_nodes(self, head):
+    stack = []
+    current = head
+
+    while current:
+        stack.append(current)
+        current = current.next
+
+    current = stack.pop()
+    maximum = current.val
+    result_list = maximum
+
+    while stack:
+        current = stack.pop()
+
+        if current.val < maximum:
+            continue
+        else:
+            new_node = current.val
+            new_node.next = result_list
+            result_list = new_node
+            maximum = current.val
+
+    return result_list
